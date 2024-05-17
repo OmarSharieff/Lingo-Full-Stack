0b2b13f61fab5848a0ce143230f0fa3170bdde0b
OmarSharieff <omarsharief642002@gmail.com>
2024-05-17 18:28:34 +0530

    Challenge Footer

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { Header } from "./header";
 import { QuestionBubble } from "./question-bubble";
 import { Challenge } from "./challenge";
+import { Footer } from "./footer";
 
 type Props = {
   initialPercentage: number;
@@ -31,10 +32,18 @@ export const Quiz = ({
 const [activeIndex, setActiveIndex] = useState(()=> {
   const uncompletedIndex = challenges.findIndex((challenge)=> !challenge.completed)
   return uncompletedIndex === -1 ? 0 : uncompletedIndex;
-
 });
+
+  const [selectedOption,setSelectedOption] = useState<number>();
+  const [status, setStatus] = useState<"correct" | "wrong" | "none">("none");
+
   const challenge = challenges[activeIndex];
   const options = challenge?.challengeOptions ?? [];
+
+  const onSelect = (id:number) => {
+    if(status !== "none") return;
+    setSelectedOption(id);
+  };
 
   const title = challenge.type === "ASSIST" ? "Select the correct meaning" : challenge.question;
   
@@ -57,9 +66,9 @@ const [activeIndex, setActiveIndex] = useState(()=> {
             )}
             <Challenge
               options={options}
-              onSelect={()=>{}}
-              status="none"
-              selectedOption={undefined}
+              onSelect={onSelect}
+              status={status}
+              selectedOption={selectedOption}
               disabled={false}
               type={challenge.type}
             />
@@ -67,6 +76,11 @@ const [activeIndex, setActiveIndex] = useState(()=> {
         </div>
       </div>
     </div>
+    <Footer
+      disabled={!selectedOption}
+      status={status}
+      onCheck={()=>{}}
+    />
   </>
   );
 };
