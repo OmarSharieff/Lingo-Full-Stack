f86de6abf3a33873269bdd32008ad8f279270e25
OmarSharieff <omarsharief642002@gmail.com>
2024-06-02 15:52:38 +0530

    Admin Dashboard for Courses

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,11 +1,32 @@
 import { NextResponse } from "next/server";
 
 import db from "@/db/drizzle";
-import { courses } from "@/db/schema";
+import { courses} from "@/db/schema";
+import { isAdmin } from "@/lib/admin";
 
 export const GET = async () => {
+  //checking if the user is admin
+  if(!isAdmin()) {
+    return new NextResponse("Unauthorized",{status: 401});
+  }
+
   //fetching all courses
   const data = await db.query.courses.findMany();
   
   return NextResponse.json(data);
+}
+export const POST = async (req: Request) => {
+  //checking if the user is admin
+  if(!isAdmin()) {
+    return new NextResponse("Unauthorized",{status: 401});
+  }
+
+  const body = await req.json();
+
+  //fetching all courses
+  const data = await db.insert(courses).values({
+    ...body,
+  }).returning();
+  
+  return NextResponse.json(data[0]);
 }
